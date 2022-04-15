f7037d7cf317105f4bd74798eca4b551651cfd3a
Cyrill <cyrillkuettel@gmail.com>
2022-04-15 18:52:25 +0200

    reset instead of pull to prevent issues

--- reset_and_rebuild.sh
+++ reset_and_rebuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 git fetch origin
-git pull origin main
+git reset --hard origin main
 docker-compose -f docker-compose-prod.yml down
 docker-compose -f docker-compose-prod.yml up --build --detach
