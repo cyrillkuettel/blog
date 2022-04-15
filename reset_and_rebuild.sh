c01d97671a6bf6a582a50c5f0a845213d578a99b
Cyrill <cyrillkuettel@gmail.com>
2022-04-15 18:55:16 +0200

    fix typo in git command

--- reset_and_rebuild.sh
+++ reset_and_rebuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 git fetch origin
-git reset --hard origin main
+git reset --hard origin/main
 docker-compose -f docker-compose-prod.yml down
 docker-compose -f docker-compose-prod.yml up --build --detach
