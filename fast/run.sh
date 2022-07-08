2c7d6ac19d954ef4d42bd77287fa468fe43d1c90
Cyrill <cyrillkuettel@gmail.com>
2022-07-08 22:55:01 +0200

    compose down on restarting

--- fast/run.sh
+++ fast/run.sh
@@ -2,4 +2,4 @@
 docker stop $(docker ps -a -q)
 docker rm $(docker ps --filter status=exited -q)
 docker build -t fast -f Dockerfile-local .
-docker run -d --name mycontainer -p 80:80 -v /home/cyrill/blog/fast/app:/code/app fast
+docker run -d --name mycontainer -p 80:80 fast ./start-reload.sh | xargs -I{} docker logs -f {}
