226da4f0569a83efa60a2cbcc2bc4cf90ec37ba9
Cyrill <cyrillkuettel@gmail.com>
2022-07-08 20:44:03 +0200

    remove duplication in deploy script

--- reset_and_rebuild.sh
+++ reset_and_rebuild.sh
@@ -3,4 +3,4 @@
 git fetch origin
 git reset --hard origin/main
 docker-compose -f docker-compose-prod.yml down
-docker-compose -f docker-compose-prod.yml up --build --detach
+/bin/bash ./run-prod.sh
