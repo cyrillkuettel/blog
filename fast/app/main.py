b248a880a2c194c30201f6657ac6715b078c2a74
Cyrill <cyrillkuettel@gmail.com>
2022-07-08 23:53:03 +0200

    update local build script

--- fast/app/main.py
+++ fast/app/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
+from pathlib import Path
+import logging
+
+current_file = Path(__file__)
+current_file_dir = current_file.parent  # /code/app
+TEMPLATES = current_file_dir / "templates"
 
 app = FastAPI()
+templates = Jinja2Templates(directory=str(TEMPLATES.resolve()))
 
 
 @app.get("/matterjs")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/main")
+async def main():
+    bla = "it works :)"
+    return templates.TemplateResponse(
+        "index.html", {"bla": bla, })  #
