57c0a1411dd2a1b8d6e5f25147b88f509e249f3c
Cyrill <cyrillkuettel@gmail.com>
2022-07-08 23:56:51 +0200

    remove unused

--- fast/run.sh
+++ fast/run.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 docker stop $(docker ps -a -q)
 docker rm $(docker ps --filter status=exited -q)
-# docker build -t fast -f Dockerfile-local .
-# docker run -d --name fastcontainre -p 80:80 fast /start-reload.sh | xargs -I{} docker logs -f {}
 docker-compose -f /home/cyrill/blog/docker-compose-prod.yml up -d python
