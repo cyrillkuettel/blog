e884c695b9bdc147578dbcea6b17b6f7cfa01571
Cyrill <cyrillkuettel@gmail.com>
2022-08-31 22:48:09 +0200

    add initial hn api.
    Note: had to modify the hn.py as per https://github.com/karan/HackerNewsAPI/pull/47/commits/fe393030190e940fb76387dba5067c6ba4cee027

--- fast/app/main.py
+++ fast/app/main.py
@@ -4,12 +4,29 @@ from starlette.requests import Request
 from starlette.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from app.core.settings import Paths
+from hn import HN
 
 app = FastAPI()
 app.mount("/app/templates", StaticFiles(directory=Paths.templates.resolve()), name="stati")
 
 templates = Jinja2Templates(directory=str(Paths.templates.resolve()))
 angry_birds_template = Jinja2Templates(directory=str(Paths.angry_birds_template_root.resolve()))
+
+
+@app.get("/app/api")
+async def api(request: Request):
+    hn = HN()
+
+    class Story:
+        def __init__(self, rank, title):
+            self.rank = rank
+            self.title = title
+
+    stories: list[Story] = []
+    for story in hn.get_stories(story_type='newest', limit=30):
+        stories.append(Story(story.rank, story.title))
+
+    return stories
 
 
 @app.get("/app/matterjs")
